e93aee97bf4b2633e37774de0078095623339096
Horseborn <79469928+Horseborn@users.noreply.github.com>
2021-04-08 14:50:12 +0200

    Unit Testing update

---
+++ Qunit.js
@@ -0,0 +1,26 @@
+let antall = 0;
+
+
+
+function plus() {
+    if (antall < 15) {
+      antall++;
+      show();
+    }
+}
+
+
+function plus2() {
+    show();
+    if (antall < 15) {
+      antall += 2;
+    }
+}
+
+function minus2() {
+    if (antall == 0 || antall == -1) {
+      
+    } else {
+      antall -= 2;
+    }
+}
